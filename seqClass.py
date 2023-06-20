bed15e86d82febd2bfbfc154cb31578541805f47
Mateo Pava <pava.mateo@gmail.com>
2023-06-20 21:18:35 +0200

    code for fix bbranch

--- seqClass.py
+++ seqClass.py
@@ -19,18 +19,15 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 
 # Convert sequence to uppercase
-args.seq = args.seq.upper()                 # Note we just added this line
+args.seq = args.seq.upper()
 
 # Check if the sequence is DNA or RNA
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print('The sequence is RNA')
-    else:
-        print('The sequence can be DNA or RNA')
+if re.search('^[ACGT]+$', args.seq):
+    print('The sequence is DNA')
+elif re.search('^[ACGU]+$', args.seq):
+    print('The sequence is RNA')
 else:
-    print('The sequence is not DNA nor RNA')
+    print('The sequence can be DNA or RNA')
 
 if args.motif:
     # Convert motif to uppercase
@@ -42,4 +39,5 @@ if args.motif:
         print("Motif found in motif branch!")
     else:
         print("Motif not found")
+
 
